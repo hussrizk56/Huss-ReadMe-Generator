b40bd500f0b3e36481576655e37b87729ef4d727
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2023-01-28 13:14:18 -0500

    updated index.js with the packages, added array of questions, and installed inquirer

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application - Added packages. 
+const inquirer = require("inquirer"); 
+const fs = require("fs");  
+const generate = require("./utils/generateMarkdown"); 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input - Added questions. 
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please provide the title name of your project."
+    }, 
+    {    
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project."
+    }, 
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions." 
+    },  
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage."
+    }, 
+    {
+        type: "list",
+        name: "license",
+        message: "Please select your project license/badge.",   
+        choices: ["ISC", "MIT", "Apache", "IBM", "BSD", "None"]
+    }, 
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please include any contributing parties."
+    }, 
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project test insturctions."
+    },
+    {
+        type: "input", 
+        name: "questions", 
+        message: "Any Questions?"    
+    },
+   {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+   }, 
+   {
+        type: "input",
+        name: "email",
+        message: "what is your email address?"
+   },
+];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file 
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) =>
+  license !== "None"
+  ? `![${license}](https://img.shields.io/static/v1?label=license&message=${license}&color=green)`
+    : "";
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  function renderLicenseLink(license) {
+    switch (license) {
+      case "ISC": 
+        return "(https://opensource.org/licenses/ISC)"; 
+
+      case "MIT":
+        return "https://opensource.org/licenses/MIT";
+  
+      case "Apache":
+        return "https://opensource.org/licenses/Apache-2.0";
+  
+      case "IBM":
+        return "https://opensource.org/licenses/IPL-1.0";
+  
+      case "BSD":
+        return "https://opensource.org/licenses/BSD-3-Clause";
+  
+      default:
+        return "";
+    }
+  };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) =>
+  license !== "None"
+  ? `[`+renderLicenseBadge(license)+`](`+renderLicenseLink(license)+`)`
+    : "";
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title}\n`+renderLicenseSection(data.license)+`
+  
+## Table of Contents: 
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [Github Username](#Username)
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+I have my ${data.license}.
+## Contributing
+${data.contributing}
+## Tests
+${data.test}
+## Questions
+${data.questions}
+## GitHub
+${data.username}
+## Email Address
+${data.email}
 `;
 }
 
